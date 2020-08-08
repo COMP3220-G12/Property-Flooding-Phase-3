df60a735ea06eeb20d81ef45d64fc01a46ec0223
LamaKha <68357513+LamaKha@users.noreply.github.com>
2020-08-08 00:57:58 -0400

    Resolved
    Resolved by adding comments to elaborate on class methods, centralized purpose and GRASP pattern. https://github.com/COMP3220-G12/Property-Flooding-Phase-3/issues/2

--- Visualizer.java
+++ Visualizer.java
@@ -1,13 +1,19 @@
+/** Visualizer an abstract class that enables us to have options to view the data
+**  For now, we will just use the text terminal view only, but we can easily plug-in any other viewing mood in the future.
+**  Using polymorphism this class helps in creating pluggable software component and handle alternatives based on types.
+**  It also supports high cohesion, as all of it's elements are supporting the same purpose.
+*/
+
 import java.util.ArrayList;
 
 abstract class Visualizer implements Template
 {
-    public Load records;
+    public Load records; // the Records loaded from a file
     public int currentRecord;
     public ArrayList<Record> newRecordList;
-
+    //abstract method	
     public abstract void displayViewer();
-
+   //constractor to instantiate a record
     public Visualizer()
     {
         records = new Load();
